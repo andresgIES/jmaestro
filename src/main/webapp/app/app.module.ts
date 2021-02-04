0ce1b02a43897082af45c51285f6bf052044c32e
andresgIES <andres.garcia@iesonline.com.co>
2021-02-04 14:06:19 -0500

    feature: se crea componente table

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -26,6 +26,7 @@ import {
     PageRibbonComponent,
     ErrorComponent
 } from './layouts';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
     imports: [
@@ -44,7 +45,8 @@ import {
         NavbarComponent,
         ErrorComponent,
         PageRibbonComponent,
-        FooterComponent
+        FooterComponent,
+        TableComponent
     ],
     providers: [
         ProfileService,
---
+++ src/main/webapp/app/table/table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-table',
+  templateUrl: './table.component.html',
+  styles: []
+})
+export class TableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
