b5d85c4341626543ddd435bf4a27953ca5325f15
andresgIES <andres.garcia@iesonline.com.co>
2021-02-04 17:14:13 -0500

    feature: se empieza maquetado de la tabla

--- src/main/webapp/app/table/table.component.ts
+++ src/main/webapp/app/table/table.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'jhi-table',
   templateUrl: './table.component.html',
-  styles: []
+  styleUrls: ['table.scss']
 })
 export class TableComponent implements OnInit {
 
