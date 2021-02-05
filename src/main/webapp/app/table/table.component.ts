29522ccee7a93047ce1f332248820cae80206c27
andresgIES <andres.garcia@iesonline.com.co>
2021-02-05 15:35:55 -0500

    feature: se añaden textos faltantes a la tabla
    style: se modifica la identacion a la izquierda de td

--- src/main/webapp/app/table/table.component.ts
+++ src/main/webapp/app/table/table.component.ts
@@ -7,17 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
-
   public numeroLoteria = 0;
   public numeroSerie = 0;
   public numeroFavorito = 0;
+  public fechaJuego: any;
+  public horaJuego: any;
 
+  constructor() { }
 
   ngOnInit() {
     this.numeroLoteria = this.numeroAleatorio(9999, 1000);
     this.numeroSerie = this.numeroAleatorio(999, 100);
     this.numeroFavorito = this.numeroAleatorio(9999, 1000);
+    this.fechaJuego = new Date(Date.now()).toLocaleString().split(',')[0];
+    this.horaJuego = this.numeroAleatorio(23, 10).toString().concat(':00');
   }
 
   private numeroAleatorio(max: number, min: number) {
